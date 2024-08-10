62fb8f132eca9d7ed5a61cbeb907fd98f884b5af
Straton12 <145207383+Straton12@users.noreply.github.com>
2024-08-10 12:15:52 +0300

    flask about and the home page

--- Flask_blog.py
+++ Flask_blog.py
@@ -4,7 +4,11 @@ app = Flask(__name__) #creating the Flask class object
  
 @app.route('/') #decorator drfines the   
 def home():  
-    return "<h1>hello, this is our first flask personal website</h1>";  
+    return "<h1>hello, this is our first flask personal website</h1>"; 
+
+@app.route('/about') #decorator drfines the   
+def about():  
+    return "<h1>about page</h1>";   
   
 if __name__ =='__main__':  
     app.run(debug = True)  
