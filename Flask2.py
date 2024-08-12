69a2bf02966bd72fd01222c6fcec7773915ccda6
Straton12 <145207383+Straton12@users.noreply.github.com>
2024-08-12 16:30:02 +0300

    my work

--- Flask2.py
+++ Flask2.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template
 
-posts = [
-    {
-        "Name": "Straton"
-        "Name": "Amodora"
-    }
-]
 
-  
 app = Flask(__name__) #creating the Flask class object   
  
 @app.route('/') #decorator drfines the   
