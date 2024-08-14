32e4672762285a1f4714c0ec495bfa58a2bbcea1
Straton12 <145207383+Straton12@users.noreply.github.com>
2024-08-14 16:20:23 +0300

    flask tutorial

--- Flask3.py
+++ Flask3.py
@@ -4,20 +4,10 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__) #creating the Flask class object   
  
 @app.route('/') #decorator drfines the   
-def home():  
+@app.route('/home')
+def home_page():  
     return render_template("home.html")
 
-@app.route('/about') #decorator drfines the   
-def about():  
-    return render_template("about.html")  
-
-#@app.route('/admin') #decorator drfines the   
-#def admin():  
-   # return redirect(url_for('home'))
-
-@app.route('/about') #decorator drfines the   
-def admin():  
-    return redirect(url_for('about.html'))
   
 if __name__ =='__main__':  
     app.run(debug = True) 
