4c814c4ee9788977e7a8342b328cd56751310643
Straton12 <145207383+Straton12@users.noreply.github.com>
2024-08-14 16:33:22 +0300

    bootsrap

--- Flask3.py
+++ Flask3.py
@@ -8,6 +8,10 @@ app = Flask(__name__) #creating the Flask class object
 def home_page():  
     return render_template("home.html")
 
+@app.route('/about') #decorator drfines the   
+def about_page():  
+    return render_template("about.html")
+
   
 if __name__ =='__main__':  
     app.run(debug = True) 
